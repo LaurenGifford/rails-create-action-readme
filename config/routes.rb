1c45bae4c85a91b21ef06b9d84b9a86bd76b7152
LaurenGifford <lauren.gifford.17@gmail.com>
2021-01-12 17:07:58 -0500

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
+  get '/posts', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  resources :posts 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
